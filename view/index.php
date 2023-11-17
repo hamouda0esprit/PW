2851871f755080612e2f77103835476429ea8fb3
valentino3015 <hellminer2003@gmail.com>
2023-11-17 01:15:12 +0100

    Added first part of the landing page

--- view/index.php
+++ view/index.php
@@ -1,10 +1,36 @@
 <!doctype html>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
+	<meta charset="utf-8">
+	<title>Index</title>
+	<link rel="stylesheet" href="../model/index.scss">
 </head>
 
 <body>
+	<div class="Main_Container">
+		<div class="first_container">
+			<div class="box">
+				<h1 class="text">Bridging the gap, One delivery at a time</h1>
+			</div>
+		</div>
+		
+		<div class="White_Line"></div>
+		
+		<div class="second_container">
+			
+		</div>
+		
+		<div class="third_container">
+			
+		</div>
+		
+		<div class="fourth_container">
+			
+		</div>
+		
+		<div class="fifth_container">
+			
+		</div>
+	</div>
 </body>
 </html>
