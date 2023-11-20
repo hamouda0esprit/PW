ad92a9b6b19e43cc96eb2c8d691bb29ab16fbe68
Emnamakni2003 <makni.emna@esprit.tn>
2023-11-20 06:44:16 +0100

    modification

--- view/font.php
+++ view/font.php
@@ -2,13 +2,14 @@
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>CodePen - Direct Messaging</title>
+  <title>Delivery Link</title>
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600" rel="stylesheet">
 
 <meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
 <link rel="stylesheet" href="./style.css">
-
+<link rel="icon" type="image/x-icon" href="./assets/logo.png">
 </head>
+
 <body>
             <?php
             require("../Model/Connection.php");
