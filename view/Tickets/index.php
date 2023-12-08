db2db87b1b7f34aea202acae28435b42417980b4
valentino3015 <hellminer2003@gmail.com>
2023-12-08 01:04:44 +0100

    Added mobile for the index

--- view/Tickets/index.php
+++ view/Tickets/index.php
@@ -1,8 +1,9 @@
 <!doctype html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Index</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8">
+
+    <title>Index</title>
 
 	<link rel="stylesheet" href="../../model/Tickets/index.scss">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
